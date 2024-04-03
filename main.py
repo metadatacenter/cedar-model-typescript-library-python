328c98f9ff3a111e87828acd7124b93024c964f0
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-04-03 11:49:29 -0700

    Generating creatin month histograms

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Step 1: Load JSON Data
-with open('../roundtrip/summary.json') as f:  # Ensure the path to your JSON file is correct
+with open('../roundtrip/summary.json') as f:
     data = json.load(f)
 
-# Convert data to a pandas DataFrame for easier manipulation
 df = pd.DataFrame(data)
 
-# Step 2: Separate Data by Type
+df['createdOnDay'] = df['createdOn'].astype(str).str[:10]
+
+df['createdOnDatetime'] = pd.to_datetime(df['createdOnDay'], errors='coerce')
+# df = df.dropna(subset=['createdOnDatetime'])
+df['createdOnYearMonth'] = df['createdOnDatetime'].dt.to_period('M')
+
 elements = df[df['type'] == 'element']
 templates = df[df['type'] == 'template']
 fields = df[df['type'] == 'field']
 
 
-# Function to create histograms with adjustments
-def create_histograms(dataframe, column, title):
-    plt.figure(figsize=(10, 6))
+def create_histograms(dataframe, column, title, file_prefix):
+    plt.figure(figsize=(19.2, 10.8))
 
-    # Adjusting the bin size for readability
     max_error = max(dataframe[column])
-    bins = np.arange(0, max_error + 1, max(1, max_error // 50))  # Ensure at least 1 to avoid zero division error
+    bins = np.arange(0, max_error + 1, max(1, max_error // 50))
 
     plt.hist(dataframe[column], bins=bins, alpha=0.7, log=True)
 
@@ -41,15 +42,35 @@ def create_histograms(dataframe, column, title):
 
     plt.grid(axis='y', alpha=0.75)
 
-    plt.savefig(f"graphs/{title.replace(':', '').replace(' ', '_').lower()}.png")  # Save the figure with a filename derived from the title
+    plt.savefig(f"graphs/{file_prefix+'_'+title.replace(':', '').replace(' ', '_').lower()}.png")
 
 
-# Step 3: Create Diagrams for Compare Error Counts
-create_histograms(elements, 'compareErrorCount', 'Elements: Comparison Error Counts')
-create_histograms(templates, 'compareErrorCount', 'Templates: Comparison Error Counts')
-create_histograms(fields, 'compareErrorCount', 'Fields: Comparison Error Counts')
+def create_histograms_by_creation_date(dataframe, title):
+    plt.figure(figsize=(19.2, 10.8))
 
-# Step 4: Create Diagrams for Parsing Error Counts
-create_histograms(elements, 'parsingErrorCount', 'Elements: Parsing Error Counts')
-create_histograms(templates, 'parsingErrorCount', 'Templates: Parsing Error Counts')
-create_histograms(fields, 'parsingErrorCount', 'Fields: Parsing Error Counts')
+    month_counts = dataframe['createdOnYearMonth'].value_counts().sort_index()
+
+    month_counts.index = month_counts.index.astype(str)
+
+    plt.bar(month_counts.index, month_counts.values, color='skyblue')
+
+    plt.title(title)
+    plt.xlabel('Creation Months')
+    plt.ylabel('Number of Artifacts')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.savefig(f"graphs/03_{title.replace(':', '').replace(' ', '_').lower()}.png")
+
+
+create_histograms(elements, 'compareErrorCount', 'Elements: Roundtrip Error Counts', '02')
+create_histograms(templates, 'compareErrorCount', 'Templates: Roundtrip Error Counts', '02')
+create_histograms(fields, 'compareErrorCount', 'Fields: Roundtrip Error Counts', '02')
+
+create_histograms(elements, 'parsingErrorCount', 'Elements: Parsing Error Counts', '01')
+create_histograms(templates, 'parsingErrorCount', 'Templates: Parsing Error Counts', '01')
+create_histograms(fields, 'parsingErrorCount', 'Fields: Parsing Error Counts', '01')
+
+create_histograms_by_creation_date(elements, 'Elements: Creation Dates')
+create_histograms_by_creation_date(templates, 'Templates: Creation Dates')
+create_histograms_by_creation_date(fields, 'Fields: Creation Dates')
